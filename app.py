03d936cc226e80da5f8041876273e8d3c3065b22
Oliver Antcliff <antcliffoc@gmail.com>
2019-10-29 16:16:01 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,65 @@
+import os
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+#setting app.config
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Book
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+# Example's of information flow with routes
+@app.route("/name/<name>")
+def get_book_name(name):
+    return "name : {}".format(name)
+
+@app.route("/details")
+def get_book_details():
+    author=request.args.get('author')
+    published=request.args.get('published')
+    return "Author : {}, Published: {}".format(author,published)
+
+@app.route("/getall")
+def get_all():
+    try:
+        books=Book.query.all()
+        return jsonify([e.serialize() for e in books])
+    except Exception as e:
+        return(str(e))
+
+@app.route("/get/<id_>")
+def get_by_id(id_):
+    try:
+        book=Book.query.filter_by(id=id_).first()
+        return jsonify(book.serialize())
+    except Exception as e:
+        return(str(e))
+
+@app.route("/add/book", methods=['GET', 'POST'])
+def add_book_form():
+    if request.method == 'POST':
+        name=request.form.get('name')
+        author=request.form.get('author')
+        published=request.form.get('published')
+        try:
+            book=Book(
+                name=name,
+                author=author,
+                published=published
+            )
+            db.session.add(book)
+            db.session.commit()
+            return "Book added. book id={}".format(book.id)
+        except Exception as e:
+            return(str(e))
+    return render_template("getdata.html")
+
+if __name__ == '__main__':
+    app.run()
